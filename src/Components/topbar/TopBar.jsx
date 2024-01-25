d1f1d1219a17bdf69e0bc759cedd87213791b4aa
Parsa Vesali <parsavesali84@gmail.com>
2024-01-25 11:56:29 +0330

    The top bar was created

---
+++ src/Components/topbar/TopBar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./TopBar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsIcon from "@mui/icons-material/Settings";
+export default function TopBar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">
+            <span>React</span> Panel
+          </span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNoneIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <LanguageIcon />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+          <img className="topbarAvatar" src="image/img-1.jfif" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
