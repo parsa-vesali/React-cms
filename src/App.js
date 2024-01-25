088c5262fba27c73354e314e5b5150b740185a44
Parsa Vesali <parsavesali84@gmail.com>
2024-01-25 11:56:07 +0330

    The top bar was created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import routes from './routes'
+import './App.css'
 import { useRoutes } from "react-router-dom";
+import TopBar from './Components/topbar/TopBar';
 export default function App() {
+
   let router = useRoutes(routes)
+
   return (
     <>
-        {router}
+      <TopBar />
+      {router}
     </>
   )
 }
